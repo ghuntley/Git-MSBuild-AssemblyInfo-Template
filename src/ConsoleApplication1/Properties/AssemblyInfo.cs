f8962a5838af494afbedada3bfa27fa13042124b
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-01-31 01:26:56 +1100

    Initial public commit

---
+++ src/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(false)]
---
+++ src/ConsoleApplication1/Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Geoffrey Huntley")]
+[assembly: AssemblyCopyright("ghuntley@ghuntley.com")]
+[assembly: AssemblyTrademark("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
